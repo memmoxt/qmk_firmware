c3bc97207cabc0d74c236af8f3d2736fca881020
MemmoXT <willianfujii@gmail.com>
2023-12-17 01:04:50 -0300

    memmoxt keymap added

---
+++ keyboards/crkbd/keymaps/memmoxt/files/current/rules.mk
@@ -0,0 +1,9 @@
+MOUSEKEY_ENABLE = yes    # Mouse keys
+RGBLIGHT_ENABLE = no   # Enable WS2812 RGB underlight.
+OLED_ENABLE     = yes
+LTO_ENABLE      = yes
+RGB_MATRIX_ENABLE = yes
+BACKLIGHT_ENABLE = no
+
+
+
