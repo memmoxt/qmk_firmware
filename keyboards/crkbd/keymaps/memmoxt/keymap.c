06c4af0fe20164fa71010fc8dc56fd2e125e52ce
MemmoXT <willianfujii@gmail.com>
2026-03-19 22:15:25 -0300

    2026 blender keymap

--- keyboards/crkbd/keymaps/memmoxt/keymap.c
+++ keyboards/crkbd/keymaps/memmoxt/keymap.c
@@ -25,7 +25,6 @@ Convert to readable JSON file: https://jhelvy.shinyapps.io/qmkjsonconverter/
 #include QMK_KEYBOARD_H
 #include <stdio.h>
 
-
 #define _LAYER0 0
 #define _LAYER1 1
 #define _LAYER2 2
@@ -36,7 +35,6 @@ Convert to readable JSON file: https://jhelvy.shinyapps.io/qmkjsonconverter/
 #define _LAYER7 7
 #define _LAYER8 8
 #define _LAYER9 9
-#define _LAYER10 10
 
 enum custom_keycodes {
     LAYER0 = SAFE_RANGE,
@@ -49,18 +47,17 @@ enum custom_keycodes {
     LAYER7,
     LAYER8,
     LAYER9,
-    LAYER10,
 };
 
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
  [_LAYER0] = LAYOUT(KC_CAPS, KC_Q, KC_W, KC_F, KC_P, KC_B, KC_J, KC_L, KC_U, KC_Y, KC_QUOT, KC_BSLS, KC_LSFT, KC_A, LALT_T(KC_R), LCTL_T(KC_S), LSFT_T(KC_T), KC_G, KC_M, LSFT_T(KC_N), LCTL_T(KC_E), LALT_T(KC_I), KC_O, KC_SCLN, KC_LGUI, KC_Z, KC_X, KC_C, KC_D, KC_V, KC_K, KC_H, KC_COMM, KC_DOT, KC_SLSH, KC_RGUI, LT(3,KC_ESC), LT(1,KC_SPC), LT(2,KC_TAB), LT(5,KC_ENT), LT(4,KC_BSPC), LT(6,KC_DEL)),
 
 [_LAYER1] = LAYOUT(TO(7), QK_BOOT, KC_NO, KC_NO, KC_NO, KC_NO, KC_AGIN, LCTL(KC_V), LCTL(KC_C), LCTL(KC_X), LCTL(KC_Z), KC_NO, TO(8), KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_NO, KC_CAPS, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO, TO(9), KC_NO, KC_RALT, KC_NO, KC_NO, KC_NO, KC_INS, KC_HOME, KC_PGDN, KC_PGUP, KC_END, KC_NO, KC_TRNS, KC_NO, KC_NO, KC_ENT, KC_BSPC, KC_DEL),
 
-[_LAYER2] = LAYOUT(KC_NO, QK_BOOT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, LCTL(KC_V), LCTL(KC_C), LCTL(KC_X), LCTL(KC_Z), KC_NO, KC_NO, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_NO, KC_NO, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_NO, KC_NO, KC_NO, KC_RALT, KC_NO, KC_NO, KC_NO, KC_NO, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, KC_NO, KC_NO, KC_NO, KC_NO, KC_BTN2, KC_BTN1, KC_BTN3),
+[_LAYER2] = LAYOUT(KC_NO, QK_BOOT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, LCTL(KC_V), LCTL(KC_C), LCTL(KC_X), LCTL(KC_Z), KC_NO, KC_NO, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_NO, KC_NO, MS_LEFT, MS_DOWN, MS_UP, MS_RGHT, KC_NO, KC_NO, KC_NO, KC_RALT, KC_NO, KC_NO, KC_NO, KC_NO, MS_WHLL, MS_WHLD, MS_WHLU, MS_WHLR, KC_NO, KC_NO, KC_NO, KC_NO, MS_BTN2, MS_BTN1, MS_BTN3),
 
-[_LAYER3] = LAYOUT(KC_NO, QK_BOOT, KC_NO, KC_NO, KC_NO, KC_NO, RGB_TOG, RGB_MOD, RGB_HUD, RGB_HUI, RGB_VAI, RGB_VAD, KC_NO, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_NO, KC_NO, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, KC_NO, KC_NO, KC_NO, KC_RALT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, RGB_SAD, RGB_SAI, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_MSTP, KC_MPLY, KC_MUTE),
+[_LAYER3] = LAYOUT(KC_NO, QK_BOOT, KC_NO, KC_NO, KC_NO, KC_NO, UG_TOGG, UG_NEXT, UG_HUED, UG_HUEU, UG_VALU, UG_VALD, KC_NO, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_NO, KC_NO, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, KC_NO, KC_NO, KC_NO, KC_RALT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, UG_SATD, UG_SATU, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_MSTP, KC_MPLY, KC_MUTE),
 
 [_LAYER4] = LAYOUT(KC_NO, KC_LBRC, KC_7, KC_8, KC_9, KC_RBRC, KC_NO, KC_NO, KC_NO, KC_NO, QK_BOOT, KC_NO, KC_NO, KC_SCLN, KC_4, KC_5, KC_6, KC_EQL, KC_NO, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, KC_NO, KC_NO, KC_GRV, KC_1, KC_2, KC_3, KC_BSLS, KC_NO, KC_NO, KC_NO, KC_RALT, KC_NO, KC_NO, KC_DOT, KC_0, KC_MINS, KC_NO, KC_NO, KC_NO),
 
@@ -68,15 +65,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_LAYER6] = LAYOUT(KC_F15, KC_F12, KC_F7, KC_F8, KC_F9, KC_PSCR, KC_F24, KC_F23, KC_F22, KC_F21, KC_F20, KC_F18, KC_F14, KC_F11, KC_F4, KC_F5, KC_F6, KC_SCRL, KC_NO, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, KC_F17, KC_F13, KC_F10, KC_F1, KC_F2, KC_F3, KC_PAUS, KC_NO, KC_NO, KC_NO, KC_RALT, KC_F19, KC_F16, KC_APP, KC_SPC, KC_TAB, KC_NO, KC_NO, KC_NO),
 
-[_LAYER7] = LAYOUT(KC_ESC, KC_NO, KC_NO, KC_NO, KC_NO, KC_BTN3, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, TO(0), KC_TAB, KC_NO, KC_LALT, KC_LCTL, KC_LSFT, KC_BTN1, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_BTN2, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, LCTL(KC_Z), KC_SPC, KC_ENT, KC_NO, KC_NO, KC_NO),
+[_LAYER7] = LAYOUT(KC_ESC, KC_F24, KC_S, KC_NO, MS_BTN1, KC_F24, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, TO(0), KC_TAB, KC_E, KC_LALT, KC_LCTL, KC_LSFT, KC_G, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_Z, KC_X, KC_Y, KC_R, MS_BTN2, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, LCTL(KC_Z), KC_SPC, KC_ENT, KC_NO, KC_NO, KC_NO),
 
 [_LAYER8] = LAYOUT(KC_ESC, LALT(KC_M), KC_D, KC_B, LCTL(KC_V), LCTL(KC_T), LCTL(KC_U), KC_NO, KC_NO, KC_NO, KC_NO, TO(0), KC_DEL, KC_A, KC_LALT, KC_LCTL, KC_LSFT, KC_E, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, LCTL(KC_C), KC_G, KC_I, KC_X, KC_L, LCTL(KC_H), KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, LCTL(KC_Z), KC_SPC, KC_ENT, LCTL(KC_S), KC_NO, KC_NO),
 
-[_LAYER9] = LAYOUT(KC_ESC, KC_GRV, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, TO(0), KC_TAB, KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_P, KC_TRNS, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, LT(10,KC_G), KC_SPC, KC_ENT, KC_BSPC, KC_DEL, KC_PSCR),
-
-[_LAYER10] = LAYOUT(KC_NO, KC_NO, KC_4, KC_5, KC_6, KC_NO, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_NO, KC_NO, KC_1, KC_2, KC_3, KC_NO, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_NO, KC_NO, KC_7, KC_8, KC_9, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO)
+[_LAYER9] = LAYOUT(KC_ESC, KC_GRV, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, TO(0), KC_TAB, KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_P, KC_TRNS, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_G, KC_SPC, KC_ENT, KC_BSPC, KC_DEL, KC_PSCR) 
 
 };
+
+
 
 #ifdef OLED_ENABLE
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
@@ -184,14 +181,6 @@ void draw_custom(void) {
 
     };
 
-    static const char PROGMEM layer10[] = {
-        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,112,112,112,112,240,240,240,112,112,240,240,240,  0,  0,  0,  0,  0,  0,  0,240,240,240,112,112,240,240,112,112,240,240,240,  0,  0,  0,  0,  0,  0,  0,240,240,240,112,112,240,240,240,112,112,240,240,  0,  0,  0,  0,  0,  0,  0, 48, 48, 48, 48,176,176,240,240,240,240,240,112,  0,  0,  0,  0,  0,  0,  0,192,224,240,112,112,112,112,112,112,112,112,112,  0,  0,  0,  0,  0,  0,  0, 48,112, 48,176,176,176,176,240,240,240,240,240,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 28, 14, 14, 14,  7, 15, 15, 14, 12, 15, 15,  7,  0,  0,  0,  0,  0,  0,  0, 15, 31, 15,  0,  0,  7,  7,  0,  0, 15, 31, 15,  0,  0,  0,  0,  0,  0,  0,  7, 15, 31, 28, 28, 15,  7,  7, 14, 15, 15,  3,  0,  0,  0,  0,  0,  0,  0, 12, 12, 12, 12, 13, 13, 13, 15, 15, 15, 15, 15,  0,  0,  0,  0,  0,  0,  0,  3,  7, 15, 14, 14, 14, 14, 14, 14, 14, 14, 14,  0,  0,  0,  0,  0,  0,  0, 15, 15, 15, 15, 15, 15, 15, 15, 14, 14, 14, 14,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-
-    };
-
 
     switch (layerID) {
         case _LAYER0:
@@ -232,10 +221,6 @@ void draw_custom(void) {
 
         case _LAYER9:
             oled_write_raw_P(layer9, sizeof(layer9));
-            break;
-
-        case _LAYER10:
-            oled_write_raw_P(layer10, sizeof(layer10));
             break;
 
         default:
